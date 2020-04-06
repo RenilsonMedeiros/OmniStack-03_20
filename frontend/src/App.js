f6cdadeaaf8aa5a811a127b817870d4972c16e45
RenilsonMedeiros <renilsonsilva@aluno.uema.br>
2020-04-06 19:22:39 -0300

    Estilização Logon feita; início das rotas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
+import './global.css'
+
+import Logon from './pages/Logon';
+
+//Um COMPONENTE no react: é uma função que retorna HTML que pode ter comandos JS e CSS.
+//JSX (JavaScript e XML): É o HTML dentro do JS
+
 function App() {
   return (
-    <h1>Hello World!</h1>
+    <Logon />
   );
 }
 
