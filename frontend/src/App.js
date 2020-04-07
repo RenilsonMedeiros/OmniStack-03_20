45e235fcf4b30f4934a96417bac9bd21a7eea15d
RenilsonMedeiros <renilsonsilva@aluno.uema.br>
2020-04-06 22:34:20 -0300

    Logon ok; Register ok; Profile beginnig

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-import './global.css'
+import './global.css';
 
-import Logon from './pages/Logon';
+import Routes from './routes';
 
 //Um COMPONENTE no react: é uma função que retorna HTML que pode ter comandos JS e CSS.
 //JSX (JavaScript e XML): É o HTML dentro do JS
 
 function App() {
   return (
-    <Logon />
+    <Routes />
   );
 }
 
